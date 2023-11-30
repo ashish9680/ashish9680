9dc583019716aaa8e0851b38bc769cea34f3563e
python project <ashish.chaurasiya9999@gmail.com>
2023-11-30 20:23:32 +0530

    Some thing changes

--- app.py
+++ app.py
@@ -1,19 +1,51 @@
-from flask import Flask
+import uuid
+from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name" : "My Store",
-        "Items": [
-            {
-                "name":"Chair",
-                "price":15.99
-            }
-        ]
-    }
-]
 
 @app.get("/stores")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        abort(404, message = "Store not found")
+    item_id = uuid.uuid4().hex
+    item = {**item_id, "id": item_id}
+    items[item_id] = item
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id, keyError=None):
+    try:
+        return stores[store_id]
+    except keyError:
+        return {"message": "Store not found"}, 404
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id, keyError=None):
+    try:
+        return stores[item_id]
+    except keyError:
+        return {"message": "Store not found"}, 404
